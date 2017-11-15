170f11637964afd463a753d0cc5559ac5da60a55
Akshay Donga <akshaydonga@gmail.com>
2017-11-15 15:04:05 +0530

    Refferal feature added

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,18 @@ class User < ApplicationRecord
       user.first_name      = user_data[:first_name]
       user.last_name       = user_data[:last_name]
       user.email           = user_data[:email]
+      user.reffer_number   = user_data[:reffer_number]
       user.coins           = 0
-
-      # if params[:reffer_number].present?
-      #   refer = user.invitations.find_by(reffer_number: params[:reffer_number])
-      #   refer.update(invitation_accept: true, invitation_accepted_at: DateTime.now)
-      # end
 
       if user.save
         otp = Otp::GenOtp.send_otp(user.contact_number) unless user.contact_number.blank?
         if otp.present? && user.update(mobile_otp: otp)
+          if user.reffer_number.present?
+            refer = User.find_by(contact_number: user.reffer_number)
+            refer_gem = refer.gems
+            refer.update(gems: refer_gem + 1)
+            user.update(gems: 1)
+          end
           result[:status]  = 200
           result[:message] = ["Success"]
         end
